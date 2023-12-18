ca71f9ea0fa669dd410835e3f76c7462df5b93bb
yhannineh <yamanhannineh@gmail.com>
2023-12-18 16:37:31 -0500

    Added the handle submit

--- golden-slice/components/Popup.js
+++ golden-slice/components/Popup.js
@@ -6,7 +6,33 @@ const Popup = ({ isOpen, onClose }) => {
     const [message, setMessage] = useState('');
     const [isSubscribed, setIsSubscribed] = useState(false);
 
+    if (!isOpen) return null;
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Form submitted");
+
+        const res = await fetch('/api/subscribe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email }),
+        });
+
+        const data = await res.json();
+        if (data.error) {
+            setMessage("Error: " + data.error);
+        } else {
+            setMessage(data.message);
+            if (data.message === "Subscription successful") {
+                setTimeout(() => {
+                    onClose();
+                }, 2000);
+            }
+            setIsSubscribed(true);
+        }
+    };
 
         
 
