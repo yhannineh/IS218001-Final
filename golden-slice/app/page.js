03f2749e6c746c7d4539ffe5b45ea379d302f50d
yhannineh <yamanhannineh@gmail.com>
2023-12-18 13:46:56 -0500

    Added the text and link

--- golden-slice/app/page.js
+++ golden-slice/app/page.js
@@ -5,5 +5,14 @@ import Link from "next/link";
 
 
 export default function Home() {
-  
+  <main>
+    <div className="hero_section flex">
+        <section className="hero_text_section">
+          <h1 className="hero_text">ENJOY TOGETHER!</h1>
+          <h2 className="hero_convince">Here&apos;s for 30% off your next order – join our exclusive rewards club now!</h2>
+          <Link className="hero_link" href="/" onClick={handleShowPopup}>GET 30% OFF</Link>
+        </section>
+        
+      </div>
+  </main>
 }
