a96dbaa53a7678936138a0b0b5940c545156c798
yhannineh <yamanhannineh@gmail.com>
2023-12-18 13:20:00 -0500

    Imported Link in page.js

--- golden-slice/app/page.js
+++ golden-slice/app/page.js
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Image from "next/image"
+import Link from "next/link";
 
 
 export default function Home() {
