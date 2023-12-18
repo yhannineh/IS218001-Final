71ce585565172752a41ea1030a98edf6459293b6
yhannineh <yamanhannineh@gmail.com>
2023-12-18 16:22:08 -0500

    Added item 6

--- golden-slice/app/menu/page.js
+++ golden-slice/app/menu/page.js
@@ -51,6 +51,15 @@ function Menu() {
                         <h2 className={styles.singlePrice}>14 / 16 / 18</h2>
                     </section>
                 </div>
+                <div className={styles.oneItem}>
+                    <section className={styles.itemText}>
+                        <h1 className={styles.name}>VEGGIE PIZZA</h1>
+                        <p className={styles.desc}>Tomato sauce, Mozzarella, Bell Peppers, Onions, Mushrooms, Black Olives</p>
+                    </section>
+                    <section className={styles.itemPrice}>
+                        <h2 className={styles.singlePrice}>14 / 16 / 18</h2>
+                    </section>
+                </div>
             </div>
       </div>
     );
