c8cc8ea0b9be03540e22391561d2c3f21fd58ec3
yhannineh <yamanhannineh@gmail.com>
2023-12-18 16:14:34 -0500

    Added menu title

---
+++ golden-slice/app/menu/page.js
@@ -0,0 +1,12 @@
+import styles from './Menu.module.css';
+
+function Menu() {
+    return (
+      <div className={styles.wholeMenu}>
+        <h1 className={styles.title}>MENU</h1>
+        
+      </div>
+    );
+  }
+  
+  export default Menu;
